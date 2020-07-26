52f2eb1d39c48484b098df83310fc901082119cf
Lova Katari <lova.katari@karhoo.com>
2020-07-26 13:22:47 +0530

    update user profile endpoint implemented postman collections updated

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,5 +1,15 @@
 const User = require('./../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+const { updatePassword } = require('./authController');
+
+const filterObj = (obj, ...allowedFields) => {
+    const newObj = {};
+    Object.keys(obj).forEach(el => {
+        if(allowedFields.includes(el)) newObj[el] = obj[el];
+    });
+    return newObj;
+}
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
     const users = await User.find();
@@ -14,7 +24,27 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
 });
 
 exports.updateMe = catchAsync(async(req, res, next) => {
-    
+    // Create error if user POSTs password data
+    if (req.body.password || req.body.passwordConfirm) {
+        return next(new AppError('This route is not for password update. Please use updateMyPassword.', 400))
+    }
+
+    // Filtered out restricted elements
+    const filteredBody = filterObj(req.body, 'name', 'email')
+
+    // Update user document
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+        new: true,
+        runValidators: true,
+    });
+
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user: updatedUser
+        }
+    })
 })
 
 exports.createUser = (req, res) => {
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -4,7 +4,8 @@ const {
     createUser,
     getUser,
     updateUser,
-    deleteUser
+    deleteUser,
+    updateMe
 } = require('./../controllers/userController');
 
 const {
@@ -27,6 +28,8 @@ router.post('/login', login);
 router.post('/forgotPassword', forgotPassword);
 router.patch('/resetPassword/:token', resetPassword);
 router.patch('/updateMyPassword', protect, updatePassword);
+
+router.patch('/updateMe', protect, updateMe);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
