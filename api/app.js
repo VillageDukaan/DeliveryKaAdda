9d71bd527009fde0eb8ecf8f0acafdfbbf76db96
Lova Katari <lova.katari@karhoo.com>
2020-07-26 20:36:50 +0530

    Implemented security best practices using middlewares

--- api/app.js
+++ api/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const ratelimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController');
@@ -9,12 +14,44 @@ const userRouter = require('./routes/userRoutes')
 const app = express();
 
 // 1) Middlewares
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if(process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+// Limit requests from one IP address
+const limiter = ratelimit({
+  max: 300,
+  windowMs:60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!' 
+})
 
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(hpp({
+  whitelist: [
+    'duration',
+    'ratingsQuantity',
+    'ratingsAverage',
+    'price'
+  ]
+}));
+
+// Test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -5,7 +5,8 @@ const {
     getUser,
     updateUser,
     deleteUser,
-    updateMe
+    updateMe,
+    deleteMe
 } = require('./../controllers/userController');
 
 const {
@@ -30,6 +31,7 @@ router.patch('/resetPassword/:token', resetPassword);
 router.patch('/updateMyPassword', protect, updatePassword);
 
 router.patch('/updateMe', protect, updateMe);
+router.delete('/deleteMe', protect, deleteMe);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
